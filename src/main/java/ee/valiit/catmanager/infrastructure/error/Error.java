497a4c550a726caa4924b4f7f6c48477d4442e6a
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-02 19:06:03 +0200

    Error enum codes 113 & 114

--- src/main/java/ee/valiit/catmanager/infrastructure/error/Error.java
+++ src/main/java/ee/valiit/catmanager/infrastructure/error/Error.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum Error {
 INCORRECT_CREDENTIALS("Vale kasutajanimi või parool", 111),
-USER_NOT_FOUND("Kasutajat ei leitud", 112);
+USER_NOT_FOUND("Kasutajat ei leitud", 112),
+USERNAME_ALREADY_EXISTS ("Kasutajanimi on juba olemas", 113),
+EMAIL_ALREADY_EXISTS ("Selle emailiga kasutaja on juba registreeritud", 114);
 
 private final String message;
 private final Integer errorCode;

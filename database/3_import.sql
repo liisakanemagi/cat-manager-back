1532a6aa8c62b5d9f369a4787048573b47a9ee6b
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-02 16:16:44 +0200

    testscript user

--- database/3_import.sql
+++ database/3_import.sql
@@ -1,6 +1,6 @@
-
+-- Kustutame vana 'liisa' kasutaja, et vältida konflikti
 DELETE FROM users WHERE username = 'liisa';
 
-
+-- Lisame kasutaja 'liisa' parooliga 'parool123' (räsitud) ja rolliga 'admin'
 INSERT INTO users (username, password, role)
-VALUES ('liisa', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOcd7QA8qkrPm', 'admin');
+VALUES ('liisa', '$2a$10$RAnl9LP9DtE0QCbi2B7gie0eWm7CASlvn2ytTxcg2XVbBzehPKoTW', 'admin');

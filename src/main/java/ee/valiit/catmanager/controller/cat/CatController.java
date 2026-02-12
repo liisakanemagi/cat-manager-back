740adb38f23c55f62b09461dc48920087a9e53e3
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-12 17:50:47 +0200

    - Retrieve User principal from SecurityContext in CatController - Update CatService.addCat signature to accept userId - Ensure service layer receives userId instead of username

--- src/main/java/ee/valiit/catmanager/controller/cat/CatController.java
+++ src/main/java/ee/valiit/catmanager/controller/cat/CatController.java
@@ -1,18 +1,25 @@
 package ee.valiit.catmanager.controller.cat;
 
-import jakarta.validation.Valid;
+ import ee.valiit.catmanager.persistence.user.User;
+ import ee.valiit.catmanager.service.CatService;
+ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-
 public class CatController {
 
-    @PostMapping("/cat")
-    public void addCat(@RequestBody @Valid CatInfo catInfo,) {
+    private final CatService catService;
 
+    @PostMapping("/cat")
+    public void addCat(@RequestBody @Valid CatInfo catInfo) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        catService.addCat(catInfo, user.getId());
     }
 }
---
+++ src/main/java/ee/valiit/catmanager/service/CatService.java
@@ -0,0 +1,14 @@
+package ee.valiit.catmanager.service;
+
+import ee.valiit.catmanager.controller.cat.CatInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CatService {
+
+    public void addCat(CatInfo catInfo, Integer  userId){
+
+    }
+}

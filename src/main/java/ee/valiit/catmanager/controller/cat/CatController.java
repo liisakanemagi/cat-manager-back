1022ae8b2e1708276bb801817528cd6cb1b50663
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-13 17:27:04 +0200

    swagger documentation for addCat

--- src/main/java/ee/valiit/catmanager/controller/cat/CatController.java
+++ src/main/java/ee/valiit/catmanager/controller/cat/CatController.java
@@ -1,10 +1,16 @@
 package ee.valiit.catmanager.controller.cat;
 
- import ee.valiit.catmanager.persistence.cat.Cat;
- import ee.valiit.catmanager.persistence.user.User;
- import ee.valiit.catmanager.service.CatService;
- import io.swagger.v3.oas.annotations.security.SecurityRequirement;
- import jakarta.validation.Valid;
+import ee.valiit.catmanager.infrastructure.error.ApiError;
+import ee.valiit.catmanager.persistence.cat.Cat;
+import ee.valiit.catmanager.persistence.user.User;
+import ee.valiit.catmanager.service.CatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,6 +26,11 @@ public class CatController {
     private final CatService catService;
 
     @PostMapping("/cat")
+    @Operation(summary = "Uue kassi lisamine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "403", description = "Oled selle nimega kassi juba lisanud (errorCode 115)", content = @Content(schema = @Schema(implementation = ApiError.class)))})
+
     public Cat addCat(@RequestBody @Valid CatInfo catInfo) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();

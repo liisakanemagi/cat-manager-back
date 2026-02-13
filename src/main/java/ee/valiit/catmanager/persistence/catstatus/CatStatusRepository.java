081ebe8ea81859162c9a2d3010eea346fa55fe8d
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-13 16:05:24 +0200

    Fix CatStatusRepository ID type to Integer & add getValidCatStatus to CatStatusService

--- src/main/java/ee/valiit/catmanager/persistence/catstatus/CatStatusRepository.java
+++ src/main/java/ee/valiit/catmanager/persistence/catstatus/CatStatusRepository.java
@@ -2,5 +2,5 @@ package ee.valiit.catmanager.persistence.catstatus;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CatStatusRepository extends JpaRepository<CatStatus, String> {
+public interface CatStatusRepository extends JpaRepository<CatStatus, Integer> {
 }
--- src/main/java/ee/valiit/catmanager/service/CatStatusService.java
+++ src/main/java/ee/valiit/catmanager/service/CatStatusService.java
@@ -1,6 +1,7 @@
 package ee.valiit.catmanager.service;
 
 import ee.valiit.catmanager.controller.catstatus.CatStatusInfo;
+import ee.valiit.catmanager.infrastructure.exception.PrimaryKeyNotFoundException;
 import ee.valiit.catmanager.persistence.catstatus.CatStatus;
 import ee.valiit.catmanager.persistence.catstatus.CatStatusMapper;
 import ee.valiit.catmanager.persistence.catstatus.CatStatusRepository;
@@ -19,6 +20,12 @@ public class CatStatusService {
     public List<CatStatusInfo> getCatStatuses(){
         List<CatStatus> catStatuses = catStatusRepository.findAll();
         return catStatusMapper.toCatStatusInfos(catStatuses);
+    }
+
+    public CatStatus getValidCatStatus(Integer statusId){
+       CatStatus catStatus = catStatusRepository.findById(statusId)
+               .orElseThrow(() -> new PrimaryKeyNotFoundException("statusId", statusId));
+       return catStatus;
     }
 }
 
